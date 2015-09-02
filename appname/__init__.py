e49feb58844db78b35034df35b603c591b205672
Liam Keene <liam.keene@gmail.com>
2015-09-02 15:53:35 +1000

    Adds Flask-Assets extension to handle compilation of CSS assets
     modified:   appname/__init__.py - register asset bundle
     new file:   appname/static/css/style.scss - stub style
     modified:   appname/templates/base.html - add asset to template
    
     modified:   requirements.txt

--- appname/__init__.py
+++ appname/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
+from flask.ext.assets import Environment, Bundle
+
+from webassets.filter import get_filter
 
 from appname.main.views import main_blueprint
 
 def create_app(object_name, env='prod'):
-    
+
     app = Flask(__name__)
 
     app.config.from_object(object_name)
     app.config['ENV'] = env
 
     app.register_blueprint(main_blueprint)
+
+    assets = Environment(app)
+    css = Bundle('css/style.scss', filters='scss', output='css/style.css')
+    assets.register('base_style', css)
 
     return app
